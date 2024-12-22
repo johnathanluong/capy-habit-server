65c61d7776a0d37ca6ff673ad50346b6434046e6
johnathanluong <johnathanluong7@gmail.com>
2024-12-22 00:16:24 -0500

    Installed gunicorn, updated requirements.txt, added JWT token managment APIs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn capy_backend.wsgi:application
--- capy_backend/urls.py
+++ capy_backend/urls.py
@@ -19,13 +19,16 @@ from django.urls import path, include
 from users.views import UserRegistration
 from habits.views import HabitViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 router = DefaultRouter()
 router.register(r'habits', HabitViewSet)
 
 urlpatterns = [
     path('register/', UserRegistration.as_view(), name='user-registration'),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token-refresh')
 ]
 
 
